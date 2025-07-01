4a88a540464713eaed2cb18c0473b840e844ac4b
Gongarveg <gonzalogarveg@gmail.com>
2025-07-01 10:38:21 +0200

    ejercicio1

---
+++ Bateria1/src/Tema1/Ejercicio1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tema1;
+
+/**
+ *
+ * @author jquin
+ */
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        System.out.println("Gonzalo");
+        System.out.println("Emparedamiento, 2, Bajo, Ecija");
+        System.out.println("681 90 98 23");
+    }
+}
