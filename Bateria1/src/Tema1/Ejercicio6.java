8b50561f03774d507a2d421a11490e5a6d4a24e0
Gongarveg <gonzalogarveg@gmail.com>
2025-07-01 11:30:03 +0200

    Create Ejercicio6.java

---
+++ Bateria1/src/Tema1/Ejercicio6.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tema1;
+
+/**
+ *
+ * @author jquin
+ */
+public class Ejercicio6 {
+    public static void main(String[] args) {
+        System.out.println("*********");
+        System.out.println(" *     *");
+        System.out.println("  *   *");
+        System.out.println("   * *");
+        System.out.println("    *");
+        
+        
+       
+        
+    }
+}
