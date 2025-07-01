a455845ebc6c17154a6b49639850e061f0c8fa0c
Gongarveg <gonzalogarveg@gmail.com>
2025-07-01 11:20:38 +0200

    ejercicio3

--- Bateria1/src/Tema1/Ejercicio3.java
+++ Bateria1/src/Tema1/Ejercicio3.java
@@ -9,5 +9,16 @@ package Tema1;
  * @author jquin
  */
 public class Ejercicio3 {
-    
+    public static void main(String[] args){
+        System.out.println("Computer \tordenador");
+        System.out.println("student \talumno/a");
+        System.out.println("cat \t\tgato");
+        System.out.println("penguin \tpinguino");
+        System.out.println("machine \tmaquina");
+        System.out.println("nature \t\tnaturaleza");
+        System.out.println("light \t\tluz");
+        System.out.println("green \t\tverde");
+        System.out.println("book \t\tlibro");
+        System.out.println("piramid \tpiramide");
+    }
 }
