5acd4ce4777d211c9603fceeb3e684053cb03d8c
Gongarveg <gonzalogarveg@gmail.com>
2025-07-01 12:13:22 +0200

    Ejercicio1

---
+++ Bateria1/src/Tema2/Ejercicio1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tema2;
+
+/**
+ *
+ * @author jquin
+ */
+public class Ejercicio1 {
+    public static void main(String[] args){
+        int x = 144;
+        int y = 999;
+        System.out.println(y + x);
+        System.out.println(y - x);
+        System.out.println(y * x);
+        System.out.println(y / x);
+        
+    }
+}
---
+++ Bateria1/src/Tema2/Ejercicio2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tema2;
+
+/**
+ *
+ * @author jquin
+ */
+public class Ejercicio2 {
+    public static void main(String[] args) {
+      String nombre = "Gonzalo Garcia Vega";
+      System.out.println(nombre);
+    }
+}
